795ff4db70c3859b56f25f2b6cfb394c6c66dcfa
mf1732020 <mf1732020@smail.nju.edu.cn>
2018-04-18 10:34:39 +0800

    back end base

---
+++ src/main/java/cn/dcan/BsfserviceApplication.java
@@ -0,0 +1,12 @@
+package cn.dcan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsfserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsfserviceApplication.class, args);
+	}
+}
